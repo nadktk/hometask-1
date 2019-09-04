f98d3faf772f20255987bb89c28ddab4ff0e1b17
nadktk <nadezhda.katkova@gmail.com>
2019-09-04 22:38:12 +0300

    task 5 fix

--- 05.js
+++ 05.js
@@ -3,6 +3,10 @@
 // solution: use strict comparison instead of type converting (if I understand the problem correctly)
 function isBig(thing) {
   //  if (thing == 0 || thing == 1 || thing == 2) {
+
+  // add type checking according to clarification from the comments
+  if (typeof thing !== 'number') return false;
+
   if (thing === 0 || thing === 1 || thing === 2) {
     return false;
   }
